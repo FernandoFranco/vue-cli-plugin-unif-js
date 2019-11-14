6bca7d638400d7048a1c02a23a2dcf073a0507df
Fernando Franco <atr.franco@gmail.com>
2019-11-14 00:43:45 -0300

    Upgrade dependencies

--- generator/index.js
+++ generator/index.js
@@ -1,8 +1,8 @@
 module.exports = (api, options) => {
   api.extendPackage({
     dependencies: {
-      'unif-js': '^1.0.0',
-      'vue-unif-js': '^1.0.0',
+      'unif-js': '^1.1.0',
+      'vue-unif-js': '^1.1.0',
     },
   });
 
